e1557a2f422be0db8668ee32b9603f99a07f951e
Alfred Stenborg <alfst109@student.liu.se>
2021-04-22 01:24:27 +0200

    Q-learning idea

--- bot/bot.js
+++ bot/bot.js
@@ -34,6 +34,29 @@ function turnAround() {
   currentDirection = (currentDirection + 2) % 4;
 }
 
+/*
+  5 actions:
+    - Up
+    - Down
+    - Left
+    - Right
+    - Boom
+
+  States:
+    - Direction of closest powerup(n, ne, nw, w, sw, s, se, e) - 8
+    - Type of block in each direction (4 directions, 4 types of blocks(unpainted, our paint, other paint, blocked))
+    - # of other players in 5x5 proximity - 1
+    - # of stunned players in 5x5 proximity - 1
+    - Do we hold a power-up? - 1
+    - Are we stunned? - 1
+    - # of total blocks painted - 1
+
+  Reward:
+    - # of total blocks painted since last state
+    - # of players stunned (+5 per player)
+    - Are we stunned? (-20)
+*/
+
 /**
  * @param {import('../src/index.js').MapUpdateEvent} mapUpdateEvent
  * @returns {Action | Promise<Action>}
